f7d2c3b9fbcaaf1d5a590b83b39de0e8c7b52d5d
quoccuong2005 <cuongnguyen2k5s@gmail.com>
2025-07-23 22:04:26 +0700

    chatroom ver 1

---
+++ chatroom/server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server)
+
+
+app.use(express.static(path.join(__dirname + "/public")))
+
+io.on("connection", (socket => {
+    socket.on("newuser", (username) => {
+        socket.broadcast.emit("update", username + " has joined the chat")
+    })
+    socket.on("exituser", (username) => {
+        socket.broadcast.emit("update", username + " has left the chat")
+    })
+    socket.on("chat", (message) => {
+        socket.broadcast.emit("chat", message)
+    })
+}))
+
+server.listen(5000)
